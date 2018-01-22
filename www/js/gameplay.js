164f99e31735bcc4b07a9c1da04e7d44396e0617
rikiki69 <laurier.alexis@gmail.com>
2018-01-22 10:38:58 +0100

    ajout jquery

--- www/js/gameplay.js
+++ www/js/gameplay.js
@@ -1,14 +1,14 @@
 //configuration du jeux
 
-var gametime = 120; //temps de jeux
-var largeur = ${'#gameSpace'}.css("width"); //largeur de la zone de jeux
+let gametime = 120; //temps de jeux
+let largeur = $('#gameSpace').css("width"); //largeur de la zone de jeux
 
 
 //Variable globale du jeu
 let count = 0; //increment l'id des ennemies
 let ingame = []; // Liste des id d'ennemies en jeux
 
-function largeuraleatoire()
+function positionLaterAlealeatoire()
 {
     return Math.round(Math.random() * largeur);
 }
@@ -16,15 +16,14 @@ function largeuraleatoire()
 function createblock()
 {
 
-  ${'#gameSpace'}.append('<div id=B' + count + ' class=enemy ></div>');
-  ${'B'+count}.offset({ top: 0, left: largeuraleatoire() });
-  ingame.append('B'+count);
+  $('#gameSpace').append('<div id=B' + count + ' class=enemy ></div>');
+  $('enemy'+count).offset({ top: 0, left: positionLaterAlealeatoire() });
+  ingame.append('enemy'+count);
 }
 
 function deleteblock(id)
 {
 ingame.splice(ingame.IndexOf(id),1);
-${id}.remove();
-
+$(id).remove();
 
 }
