515cbaa565ac4fbcbe4f58187ce455029b1dfe6c
rikiki69 <laurier.alexis@gmail.com>
2018-01-22 12:29:52 +0100

    ca descend mieux mais s'arrete trop tot

--- www/js/gameplaysuite.js
+++ www/js/gameplaysuite.js
@@ -12,22 +12,21 @@ function inGame()
 {
   for (let id in ingame) {
     console.log($(ingame[id]).css("top"));
-    if($(ingame[id]).css("top")>0)
+    if($(ingame[id]).css("top").substring(0,$(ingame[id]).css("top").length-2)<100)
     {
-      $(ingame[id]).css("top","+=-1%");
+      $(ingame[id]).css("top","+=1%");
     }
-$(ingame[id]).css("top","+=1px");
 
 //elseif gestion des collisions.
 
-  //  else {
-  //    deleteblock(ingame[id]);
-  //  }
+else {
+     deleteblock(ingame[id]);
+   }
 
   }
 
 
 }
-var intervalcreate = setInterval(createblock,1000);
+var intervalcreate = setInterval(createblock,3000);
 var interval = setInterval(inGame,50);
 document.addEventListener("keydown", deplacement, false);
