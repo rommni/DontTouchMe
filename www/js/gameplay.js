3f9b1e5b29d511791db720e9c255c50f3d907376
rikiki69 <laurier.alexis@gmail.com>
2018-01-22 10:59:36 +0100

    deplacement non fonctionnel

--- www/js/gameplay.js
+++ www/js/gameplay.js
@@ -1,12 +1,13 @@
 //configuration du jeux
 
 let gametime = 120; //temps de jeux
-let largeur = $('#gameSpace').css("width"); //largeur de la zone de jeux
+let largeur = $("#gameSpace").css("width"); //largeur de la zone de jeux
 
 
 //Variable globale du jeu
 let count = 0; //increment l'id des ennemies
 let ingame = []; // Liste des id d'ennemies en jeux
+
 
 function positionLaterAlealeatoire()
 {
@@ -16,9 +17,9 @@ function positionLaterAlealeatoire()
 function createblock()
 {
 
-  $('#gameSpace').append('<div id=B' + count + ' class=enemy ></div>');
-  $('enemy'+count).offset({ top: 0, left: positionLaterAlealeatoire() });
-  ingame.append('enemy'+count);
+  $("#gameSpace").append("<div id=B" + count + " class=enemy ></div>");
+  $("enemy"+count).offset({ top: 0, left: positionLaterAlealeatoire() });
+  ingame.append("enemy"+count);
 }
 
 function deleteblock(id)
@@ -27,3 +28,43 @@ ingame.splice(ingame.IndexOf(id),1);
 $(id).remove();
 
 }
+
+function deplacergauche()
+{
+
+if($("player").css("left")>0)
+{
+  $("player").css("left","-=1px");
+}
+
+}
+
+function deplacerdroite()
+{
+
+if($("player").css("left")< largeur )
+{
+  $("player").css("left","+=1px");
+}
+
+}
+
+function deplacement(event) {
+    var x = event.which || event.keyCode; // event.keyCode is used for IE8 and earlier
+    if (x == 27) {  // 27 is the ESC key
+        deplacergauche();
+    }
+    if (x == 39) {  // 27 is the ESC key
+        deplacerdroite();
+    }
+}
+
+/// Zone de gestion du jeux
+function inGame()
+{
+
+
+
+}
+
+var interval = setInterval(inGame(),50);
